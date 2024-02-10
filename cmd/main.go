ec5732ad4468bff4ce26f2a8aaf58f7c8db475d0
João Victor da Silva <jv782063@gmail.com>
2024-02-09 21:47:31 -0300

    configure .env

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "github.com/joaovds/rinha-crebito/configs"
+
 func main() {
+	configs.LoadEnv()
+
   println("Hello, World!")
 }
---
+++ configs/env.go
@@ -0,0 +1,46 @@
+package configs
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Env struct {
+	Port        string
+}
+
+var ENV *Env
+
+const (
+	DefaultPort = "3333"
+)
+
+func newEnv() *Env {
+	port := DefaultPort
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+
+	return &Env{
+		Port:        port,
+	}
+}
+
+func LoadEnv(filepath ...string) {
+	if len(filepath) > 1 {
+		log.Panic("You can only pass one filepath to LoadEnv")
+	}
+
+	if len(filepath) == 0 {
+		filepath = append(filepath, ".env")
+	}
+
+	err := godotenv.Load(filepath[0])
+	if err != nil {
+		log.Panic("Error loading .env file:", err)
+	}
+
+	ENV = newEnv()
+}
