7ee67a98758e927c7149ce9da8ea3a0d3c481122
João Victor da Silva <jv782063@gmail.com>
2024-02-09 21:33:38 -0300

    init domain

---
+++ internal/domain/account.go
@@ -0,0 +1,19 @@
+package domain
+
+type Account struct {
+  ID int
+  Limit int
+  Balance int
+}
+
+func NewAccount(id, limit, balance int) *Account {
+  return &Account{
+    ID: id,
+    Limit: limit,
+    Balance: balance,
+  }
+}
+
+type AccountRepository interface {
+  GetByID(id int) (*Account, error)
+}
