1ab4bfcb0bd641fb53cd8c1763a07df9c26f51db
João Victor da Silva <jv782063@gmail.com>
2024-02-26 17:42:18 -0300

    starts handleFunc of routes

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"log"
+	"net/http"
 )
 
 func main() {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/clientes/{id}/transacoes", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		if r.PathValue("id") == "" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, client " + r.PathValue("id")))
+	})
+
+	mux.HandleFunc("/clientes/{id}/extrato", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		if r.PathValue("id") == "" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, client " + r.PathValue("id")))
+	})
+
 	log.Println("Starting server on http://localhost:9999")
+	log.Fatal(http.ListenAndServe(":9999", mux))
 }
