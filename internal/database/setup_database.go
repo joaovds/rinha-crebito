674cc14b292bc90ade39229b49a1ce98f16b50fe
João Victor da Silva <jv782063@gmail.com>
2024-02-29 23:14:12 -0300

    configure nginx

--- internal/database/setup_database.go
+++ internal/database/setup_database.go
@@ -11,7 +11,14 @@ import (
 var Db *pgxpool.Pool
 
 func SetupDatabase() (*pgxpool.Pool, error) {
-	dbPool, err := pgxpool.New(context.Background(), "postgres://root:root@db:5432/rinha")
+  dbConfig, err := pgxpool.ParseConfig("postgres://root:root@127.0.0.1:5432/rinha")
+  if err != nil {
+    log.Fatalf("Unable to parse database config: %v\n", err)
+  }
+  dbConfig.MaxConns = 100
+  dbConfig.MinConns = 10
+
+	dbPool, err := pgxpool.NewWithConfig(context.Background(), dbConfig)
 	if err != nil {
 		log.Fatalf("Unable to connect to database: %v\n", err)
 		os.Exit(1)
